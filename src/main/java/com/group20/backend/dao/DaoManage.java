5a8770f52206f62988b97705b906d820d8887234
nx-xn2002 <1870230468@qq.com>
2024-05-27 18:04:16 +0800

    update

--- src/main/java/com/group20/backend/dao/DaoManage.java
+++ src/main/java/com/group20/backend/dao/DaoManage.java
@@ -8,11 +8,13 @@ import com.group20.backend.model.Money;
  * @author Ni Xiang
  */
 public class DaoManage {
-    private static final UserDao USER_DAO = new UserDao("src/main/resources/user_data.csv");
-    private static final RelationDao RELATION_DAO = new RelationDao("src/main/resources/relation_data.csv");
-    private static final AccountDao ACCOUNT_DAO = new AccountDao("src/main/resources/account_data.csv");
-    private static final MoneyDao MONEY_DAO = new MoneyDao("src/main/resources/money_data.csv");
-    private static final TaskDao TASK_DAO = new TaskDao("src/main/resources/task_data.csv");
+    private static final String PATH_FRONT1 = "src/main/resources/";
+    private static final String PATH_FRONT2 = "";
+    private static final UserDao USER_DAO = new UserDao(PATH_FRONT1 + "user_data.csv");
+    private static final RelationDao RELATION_DAO = new RelationDao(PATH_FRONT1 + "relation_data.csv");
+    private static final AccountDao ACCOUNT_DAO = new AccountDao(PATH_FRONT1 + "account_data.csv");
+    private static final MoneyDao MONEY_DAO = new MoneyDao(PATH_FRONT1 + "money_data.csv");
+    private static final TaskDao TASK_DAO = new TaskDao(PATH_FRONT1 + "src/main/resources/task_data.csv");
 
     public static UserDao getUserDaoInstance() {
         return USER_DAO;
